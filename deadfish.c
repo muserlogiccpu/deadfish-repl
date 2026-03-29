c2689658658d12bd609d1d53aab75c246256b06e
muserlogiccpu <developmentmuser@gmail.com>
2026-03-29 12:30:18 +0300

    Update deadfish.c
    Fixed issues, and made UX design better.

--- deadfish.c
+++ deadfish.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <limits.h>
 int main() {
 	int c;
 	int acc = 0;
 	while (1) {
 		printf("deadfish repl >>> ");
 		c = getchar();
-		if (c == '\n' || c == EOF) continue;
-		if (c == 'i') acc++;
-		else if (c == 'd') acc--;
-		else if (c == 's') acc = acc * acc;
-		else if (c == 'o') printf("%d\n",acc);
+		if (c == EOF) break;
+		if (c == '\n') continue;
+		if (c == 'i') {
+			if (acc == INT_MAX) acc = 0;
+			else acc++;
+		}
+		else if (c == 'd') {
+			acc--;
+		}
+		else if (c == 's') {
+			// anti overflow system
+			if (acc > 0 && acc > INT_MAX / acc) acc = 0;
+			else acc = acc * acc;
+		}
+		else if (c == 'o') printf("\n%d",acc);
+		else if (c == 'h') break;
 		if (acc == 256 || acc == -1) acc = 0;
+		printf("\n");
 	}
 }
