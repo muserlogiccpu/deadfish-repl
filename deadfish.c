426750662bcce76ff922e66019680fbbd3abec47
muserlogiccpu <developmentmuser@gmail.com>
2026-03-29 15:50:40 +0300

    Update deadfish.c
    Using now deadfish.h

--- deadfish.c
+++ deadfish.c
@@ -1,29 +1,16 @@
-#include <stdio.h>
-#include <limits.h>
+#include "deadfish.h"
 int main() {
-	int c;
-	int acc = 0;
-	printf("deadfish repl >>> ");
-	fflush(stdout);
-	while ((c = getchar()) != EOF) {
-		if (c == 'i') {
-			if (acc == INT_MAX) acc = 0;
-			else acc++;
-		}
-		else if (c == 'd') {
-			acc--;
-		}
-		else if (c == 's') {
-			// anti overflow system
-			if (acc > 0 && acc > INT_MAX / acc) acc = 0;
-			else acc = acc * acc;
-		}
-		else if (c == 'o') printf("%d\n",acc);
-		else if (c == 'h') break;
-		if (acc == 256 || acc == -1) acc = 0;
-		if (c == '\n') {
-			printf("deadfish repl >>> ");
-			fflush(stdout);
-		};
-	}
+    int c;
+    int acc = 0;
+    printf("(%d) >> ", acc);
+    fflush(stdout);
+    while ((c = getchar()) != EOF) {
+        if (c == 'h') break;
+        df_step(&acc, (char)c);
+        if (c == '\n') {
+            printf("(%d) >> ", acc);
+            fflush(stdout);
+        }
+    }
+    return 0;
 }
